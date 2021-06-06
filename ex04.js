5f200d7541f56327afce7aaeba19e1e2fbd37e61
MouraVocal <diego.moura3@gmail.com>
2021-06-05 22:19:12 -0300

    classe com funções criada

---
+++ ex04.js
@@ -0,0 +1,36 @@
+/**
+ * -Crie uma classe que simule um carro;
+ * -Propriedades: marca, cor, gasolina restante;
+ * -Crie um método de dirigir o carro, que vá diminuindo a gasolina gradativamente;
+ * -E um de abastecer para aumentar a gasolina quando necessário;
+ */
+class Car {
+    constructor(mark, color, remainingGasoline, kilometersPerLiter, gasCost){
+        this.mark = mark,
+        this.color = color,
+        this.remainingGasoline = remainingGasoline,
+        this.kilometersPerLiter = kilometersPerLiter,
+        this.gasCost = gasCost
+    }
+    driveCar(kilometersTraveled) {
+        //kilometersPerLiter = kilometersTraveled / Liter   
+        this.remainingGasoline -= Math.round(kilometersTraveled / this.kilometersPerLiter).toFixed(0);
+        let moneyCost = (kilometersTraveled / this.kilometersPerLiter) *this.gasCost
+        console.log(`Voce gastou ${Math.round(moneyCost).toFixed(2)} reais`)
+        if(this.remainingGasoline < 10){
+            console.log('Hora de abastecer!');
+        }
+    }
+    refuel(howMuch){
+        this.remainingGasoline += howMuch
+        console.log(`O tanque foi abastecido em ${howMuch}, o tanque está com ${this.remainingGasoline} litros`)
+    }
+}
+
+let meuCarro = new Car('Citroen C3', 'preto', 48, 12.4, 4.83);
+
+console.log(meuCarro);
+
+meuCarro.driveCar(50.3*2);
+
+console.log(meuCarro);
